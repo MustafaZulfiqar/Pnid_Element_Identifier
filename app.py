44684ca5bc35a571989f3f48452a8f9290614df6
MustafaZulfiqar <50071509+MustafaZulfiqar@users.noreply.github.com>
2026-02-07 20:56:54 +0000

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from io import BytesIO
 import google.generativeai as genai
 
 st.title("LLM File Processor")
-genai.configure(api_key="AIzaSyAPF_63muYnIY0EgSVJukw7v49f3oLJPtE")
+genai.configure(api_key="AIzaSyCsy6rECIf_4xT9_Bnp9eooqrm47gM3GYw")
 
 model = genai.GenerativeModel("Default Gemini API Key")
 
